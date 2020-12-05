f0fd392bcdf97fb815982efaf85f6480b3126eb6
Kacper Kuźniarski <kacper.kuzniarski@cloudservices.com>
2020-12-06 00:05:08 +0100

    docker build trigger

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String,)
     last_name = Column(String)
